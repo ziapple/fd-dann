0d28aca0d0810e8c6fceaac52138330c89147509
ziapple <ziapple@126.com>
2021-05-29 19:23:40 +0800

    split tain dataset and test dataset

--- dataset/data_loader_emd.py
+++ dataset/data_loader_emd.py
@@ -7,44 +7,29 @@ from . import data_conf as conf
 
 # emd数据加载器，一行一个样本(batch_size, (x_height = 7 * x_width = 400))
 class GetLoader(data.Dataset):
-    def __init__(self, data_root, file_name, transform=None):
+    def __init__(self, data_root, data_dir, transform=None):
         self.root = data_root
         self.transform = transform
 
-        # 读取文件
-        x = np.loadtxt(file_name)
-        # 读取样本，还原
-        x = x.reshape(-1, conf.EMD_IMF_SIZE, conf.X_LEN)
-        # 将IMF作为深度变换（类似图片的RGB），转化成1*X_HEIGHT*X_LEN
-        x_1 = np.zeros((x.shape[0], 1, conf.EMD_IMF_SIZE, conf.X_LEN))
-        for j in range(x.shape[0]):
-            # 每个样本做一个转置
-            x_1[j, 0] = x[i].reshape(EMD_IMF_SIZE, X_LEN).T
-        # 取前IMF_X_LENGTH个
-        x_1 = x_1[:, :, :, :X_HEIGHT]
-        n = x_1.shape[0]
-        # 按3/4比例为训练数据，1/4为测试数据
-        n_split = int((3 * n / 4))
-        # 二维数组填充,增量式填充
-        x_train = np.vstack((x_train, x_1[:n_split]))
-        x_test = np.vstack((x_test, x_1[n_split:]))
-        # [0]+[1] = [0, 1],不断累积标签
-        y_train += [i] * n_split
-        y_test += [i] * (x.shape[0] - n_split)
-        i += 1
-        f = open(os.path.join(data_root, file_name))
+        # 加载目录下的所有文件
+        for item in os.listdir(os.path.join(data_root, data_dir)):
+            x = np.loadtxt(item)
+            # 读取样本，还原
+            x = x.reshape(-1, conf.X_LEN, conf.EMD_IMF_SIZE)
+            # B是球体故障0，IR是内环故障1，OR是外环故障2
+            y = -1
+            if item.find("B") > 0:
+                y = 0
+            elif item.find("IR") > 0:
+                y = 1
+            elif item.find("OR") > 0:
+                y = 2
 
-        data_list = f.readlines()
-        f.close()
-
-        self.n_data = len(data_list)
-
-        self.emd_data = []
-        self.emd_labels = []
-
-        for s_data in data_list:
-            self.emd_data.append(s_data[:-3])
-            self.emd_labels.append(s_data[-2])
+            # 存储数据
+            self.emd_data = []
+            self.emd_labels = []
+            self.emd_data.extend(x)
+            self.emd_labels.append([y]*len(x))
 
     def __getitem__(self, item):
         img_paths, labels = self.img_paths[item], self.img_labels[item]
--- dataset/data_reader.py
+++ dataset/data_reader.py
@@ -3,46 +3,56 @@ import numpy as np
 import random
 import os
 from PyEMD import EEMD
-from . import data_conf as conf
+import data_conf as conf
 
 
 # mat文件为采样样本，采样频率为12k，10秒采集一次，生成一个文件，共samples=122571采集点
 # 把这samples大样本数据变成每行是EMD_IMF_SIZE*X_LEN的数据，每行可以看成是一张二维图片（高7，宽400），存储成一维数据
 # 把samples样本切分成122571/X_LEN份，每份emd_sample样本长度为X_LEN，emd_sample经过emd转化为EMD_IMF_SIZE*X_LEN二维的一个数组
 # 每个emd_sample二维样本数据，可以看成是每张图片样本数据
-
-
-def save_data_to_emd():
+def save_data_to_emd(file_name, data_type):
     """
-    读取data下面所有文件,每个mat文件对应一个emd分量文件
+    读取cwru下面以.mat结尾的文件，并转化为emd分量文件
     :param
+        file_name: 文件名前缀
+        data_type: 读取的文件类型:驱动端|风扇端
     :return:
     """
     eemd = EEMD()
     emd = eemd.EMD
     emd.extrema_detection = "parabol"
     for item in os.listdir(conf.DATA_DIR):
-        print("start...{%s}" % item)
-        short_name, _ = os.path.splitext(item)
-        time_series = read_mat_data(os.path.join(conf.DATA_DIR + item))
-        # 将故障样本拆分为训练样本长度
-        input_length = time_series.shape[0]//X_LEN
-        # 三维矩阵，记录每个输入样本长度，每个分量，信号信息,
-        x = np.zeros((input_length, conf.EMD_IMF_SIZE, conf.X_LEN))
-        # 获取序列最大长度，去掉信号后面的余数
-        idx_last = -(time_series.shape[0] % conf.X_LEN)
-        # 切片分割(input_length, x_len)
-        clips = time_series[:idx_last].reshape(-1, conf.X_LEN)
-        # 对每个样本切片做eemd处理
-        for i in range(input_length):
-            # 经过emd分解后的imf_size可能为6,7,8，统一取>=7的EMD_IMF_SIZE
-            imf = eemd.eemd(clips[i])
-            if imf.shape[0] >= conf.EMD_IMF_SIZE:
-                x[i] = imf[:conf.EMD_IMF_SIZE]
-        # x变成input_length个样本，(input_length, emd_imf_size = 7 * x_len = 400)
-        a = x.reshape(input_length, -1)
-        np.savetxt(os.path.join(conf.DATA_DIR + str(conf.X_LEN) + "/", short_name + '.emd'), a)
-        print("save success {%s}", os.path.join(conf.DATA_DIR + str(conf.X_LEN) + "/", short_name + '.emd'))
+        if item.endswith(".mat") and item.startswith(file_name):
+            print("start...{%s}" % item)
+            short_name, _ = os.path.splitext(item)
+            time_series = read_mat_data(os.path.join(conf.DATA_DIR + item))
+            # 将故障样本拆分为训练样本长度
+            input_length = time_series.shape[0]//conf.X_LEN
+            # 三维矩阵，记录每个输入样本长度，每个分量，信号信息,
+            x = np.zeros((input_length, conf.EMD_IMF_SIZE, conf.X_LEN))
+            # 获取序列最大长度，去掉信号后面的余数
+            idx_last = -(time_series.shape[0] % conf.X_LEN)
+            # 切片分割(input_length, x_len)
+            clips = time_series[:idx_last].reshape(-1, conf.X_LEN)
+            # 对每个样本切片做eemd处理
+            for i in range(input_length):
+                # 经过emd分解后的imf_size可能为6,7,8，统一取>=7的EMD_IMF_SIZE
+                imf = eemd.eemd(clips[i])
+                if imf.shape[0] >= conf.EMD_IMF_SIZE:
+                    x[i] = imf[:conf.EMD_IMF_SIZE]
+            # x变成input_length个样本，(input_length, emd_imf_size = 7 * x_len = 400)
+            a = x.reshape(input_length, -1)
+            # 按4:3比例存取训练数据和测试数据
+            # 按3/4比例为训练数据，1/4为测试数据
+            n_split = int((3 * len(a) / 4))
+            # 二维数组填充,增量式填充
+            x_train = [], x_test = []
+            x_train = np.vstack((x_train, a[:n_split]))
+            x_test = np.vstack((x_test, a[n_split:]))
+            np.savetxt(os.path.join(conf.DATA_DIR + data_type + "/train/", short_name + '.emd'), x_train)
+            print("save success {%s}", os.path.join(conf.DATA_DIR + data_type + "/train", short_name + '.emd'))
+            np.savetxt(os.path.join(conf.DATA_DIR + data_type + "/test/", short_name + '.emd'), x_test)
+            print("save success {%s}", os.path.join(conf.DATA_DIR + data_type + "/test", short_name + '.emd'))
 
 
 def read_mat_data(f_path):
@@ -76,40 +86,8 @@ def _shuffle(x, y):
     return x, y
 
 
-def read_emd():
-    """
-    读取转化后的emd_data文件,类似图片处理，每个样本按照(1, x_height, x_len)维度返回
-    :return:
-    """
-    x_train = np.zeros((0, 1, conf.X_HEIGHT, conf.X_LEN))
-    x_test = np.zeros((0, 1, conf.X_HEIGHT, conf.X_LEN))
-    y_train = []
-    y_test = []
-    i = 0
-    for item in os.listdir(os.path.join(conf.DATA_DIR, str(conf.X_LEN))):
-        print("read %s" % item)
-        x = np.loadtxt(conf.DATA_DIR + str(conf.X_LEN) + "/" + item)
-        # 读取样本，还原
-        x = x.reshape(-1, conf.X_HEIGHT, conf.X_LEN)
-        # 将IMF作为深度变换（类似图片的RGB），转化成1*X_HEIGHT*X_LEN
-        x_1 = np.zeros((x.shape[0], 1, conf.EMD_IMF_SIZE, conf.X_LEN))
-        for j in range(x.shape[0]):
-            # 每个样本做一个转置
-            x_1[j, 0] = x[i].reshape(conf.EMD_IMF_SIZE, conf.X_LEN).T
-        # 取前X_LENGTH个
-        x_1 = x_1[:, :, :, :conf.X_HEIGHT]
-        n = x_1 .shape[0]
-        # 按3/4比例为训练数据，1/4为测试数据
-        n_split = int((3 * n / 4))
-        # 二维数组填充,增量式填充
-        x_train = np.vstack((x_train, x_1[:n_split]))
-        x_test = np.vstack((x_test, x_1[n_split:]))
-        # [0]+[1] = [0, 1],不断累积标签
-        y_train += [i] * n_split
-        y_test += [i] * (x.shape[0] - n_split)
-        i += 1
-    return x_train, y_train, x_test, y_test
-
-
 if __name__ == "__main__":
-    save_data_to_emd()
+    # 驱动端作为源数据
+    # save_data_to_emd("12K_Drive", "source")
+    # 风扇端作为模型要迁移目标数据
+    save_data_to_emd("12K_Fan", "target")
